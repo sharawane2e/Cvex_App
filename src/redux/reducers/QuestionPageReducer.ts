13f32bd39395eb5382b816df38c8b759de45fda4
ashishgangware2e <79965216+ashishgangware2e@users.noreply.github.com>
2022-06-09 20:48:10 +0530

    css update

--- src/redux/reducers/QuestionPageReducer.ts
+++ src/redux/reducers/QuestionPageReducer.ts
@@ -2,15 +2,15 @@ import { createReducer } from '@reduxjs/toolkit';
 import { setAnswerCount } from '../actions/QuestionPageAction';
 
 const initialState: {
-  progressBar: any;
+  answerCount: any;
 } = {
-  progressBar: 0,
+  answerCount: 0,
 };
 
 const QuestionPage = createReducer(initialState, (builder) => {
   builder.addCase(setAnswerCount, (state, action) => ({
     ...state,
-    progressBar: action.payload,
+    answerCount: action.payload,
   }));
 });
 
