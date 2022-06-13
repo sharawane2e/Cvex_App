7f48b5ec8cb3b128d7e6baff22800ad7edf9df0d
Gaurav Jain <gaurav.jain@e2eresearch.com>
2022-06-13 21:13:28 +0530

    updated new json

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -12,39 +12,41 @@ import PanelPage from "../components/PanelPage";
 const Pages = () => {
   const [jsonData, setJSONData] = useState<any>('');
 
-  // useEffect(() => {
-  //   setJSONData(
-  //     // @ts-ignore
-  //     JSON.parse(document.getElementById('jsonData')?.innerHTML),
-  //   );
-  // }, []);
+  useEffect(() => {
+    setJSONData(
+      // @ts-ignore
+      JSON.parse(document.getElementById('jsonData')?.innerHTML),
+    );
+  }, []);
 
-  // if (jsonData != '' || jsonData != undefined) {
-  //   if (jsonData?.pageCode?.page == 1) {
-  //     return <Login />;
-  //   }
-  //   if (jsonData?.pageCode?.page == 2) {
-  //     return <Introduction />;
-  //   }
-  //   if (jsonData?.pageCode?.page == 3) {      
-  //     return <GI />;
-  //   }
-  //   if (jsonData?.pageCode?.page == 4) {
-      
-  //     return <QuestionPage />;
-  //   }
-  //   if (jsonData?.pageCode?.page == 5) {
-      
-  //     return <ThankYou />;
-  //   }
-  //   if (jsonData?.pageCode?.page == 6) {
-      
-  //     return <PanelPage />
-  //   }
-  // }
+  console.log(jsonData?.pageCode?.page)
 
-  return <PanelPage /> 
+  if (jsonData != '' || jsonData != undefined) {
+    if (jsonData?.pageCode?.page == 1) {
+      return <Login />;
+    }
+    if (jsonData?.pageCode?.page == 2) {
+      return <Introduction />;
+    }
+    if (jsonData?.pageCode?.page == 3) {
+      return <GI />;
+    }
+    if (jsonData?.pageCode?.page == 4) {
 
- 
+      return <QuestionPage />;
+    }
+    if (jsonData?.pageCode?.page == 5) {
+
+      return <ThankYou />;
+    }
+    if (jsonData?.pageCode?.page == 6) {
+
+      return <PanelPage />
+    }
+  }
+
+  // return <PanelPage /> 
+
+
 };
 export default Pages;
