5e88f82bc8825458ee2a8e0e7e0258985f91f819
Gaurav Jain <gaurav.jain@e2eresearch.com>
2022-06-16 19:28:56 +0530

    update on new dev

--- src/components/ImpactCalculator/HsddInput.tsx
+++ src/components/ImpactCalculator/HsddInput.tsx
@@ -8,133 +8,10 @@ type HsddInputProps = {
 const HsddInput = (props: HsddInputProps) => {
 
 	const { optionName, type, placeholder, description, questionId, selectedId, enableQuestionIds, options, onChange } = props.question;
-	// const [jsonData, setJSONData] = useState<any>('');
-
-	// useEffect(() => {
-	// 	setJSONData(
-	// 		// @ts-ignore
-	// 		JSON.parse(document.getElementById('jsonData')?.innerText),
-	// 	);
-	// }, []);
-	// const inputDetails = jsonData?.data?.inputData;
-
-	// const getselectedDDName = (options: any, selectedId: string) => {
-	// 	let selectedDDName = '';
-	// 	options.forEach((element: any) => {
-	// 		if (element.ddId == selectedId) {
-	// 			selectedDDName = element.ddName;
-	// 		}
-	// 	});
-	// 	// inputDetails?.forEach((inputDetail: any) => {
-	// 	// 	inputDetail?.subHeadingDetails?.forEach((subHeadingDetail: any) => {
-	// 	// 		subHeadingDetail?.segmentDetails?.forEach((segmentDetail: any) => {
-	// 	// 			segmentDetail?.questions?.forEach((question: any) => {
-	// 	// 				question?.options.forEach((option: any) => {
-	// 	// 					if (option.ddId == selectedId) {
-	// 	// 						selectedDDName = option.ddName;
-	// 	// 					}
-	// 	// 				})
-	// 	// 			});
-	// 	// 		});
-	// 	// 	});
-	// 	// });
-	// 	return selectedDDName;
-	// };
-	// console.log(inputDetails.subHeadingDetails)
-	// console.log(props?.selectedId)
-
-	// const hsddRender = () => {
-	// 	if (props.type == "hsdd") {
-	// 		return (
-	// 			<>
-	// 				<Grid container sx={{ alignItems: 'center', }} xs={12} md={4} lg={4}
-	// 				className="input-form-control">
-	// 					{/* <Grid item xs={12} md={6} className="single-dropdown-title">
-	// 						<p>{props?.optionName}</p>
-	// 					</Grid> */}
-	// 					<Grid item xs={12} >
-	// 					<p className="label-heading">{props?.optionName}</p>
-	// 						<FormControl fullWidth>
-	// 							<Select
-	// 								sx={{ p: 0, borderRadius: 0, mb: 1 }}
-	// 								className="inputField cutom-input-field"
-	// 								defaultValue="none"
-	// 								value={getselectedDDName(
-	// 									props.menuItems, props.selectedId
-	// 								)}
-	// 								onChange={(e) => {
-	// 									let dropdownId = '';
-	// 									if (props.type == "hsdd") {
-	// 										props.menuItems.forEach((option: any) => {
-	// 											if (option.ddName == e.target.value) {
-	// 												dropdownId = option.ddId;
-	// 												// document.getElementById(option.ddId)?.click();
-	// 											}
-	// 										})
-	// 									}
-	// 									const updatedQuestionsArray: any[] = [];
-	// 									inputDetails?.forEach((inputDetail: any) => {
-	// 										inputDetail?.subHeadingDetails.forEach((subHeadingDetail: any) => {
-	// 											subHeadingDetail?.segmentDetails.forEach((segmentDetail: any) => {
-	// 												segmentDetail?.questions.forEach((question: any) => {
-	// 													if (props.type == "hsdd") {
-	// 														if (question.questionId === props.questionId) {
-	// 															question.selectedId = dropdownId;
-	// 														}
-	// 														updatedQuestionsArray.push(inputDetail);
-	// 													}
-	// 												});
-	// 											});
-	// 										});
-	// 									});
-	// 									setJSONData({
-	// 										...jsonData, data: { ...jsonData.rightData, inputData: [...updatedQuestionsArray] }
-	// 									});
-	// 								}}
-	// 							>
-	// 								<MenuItem
-	// 									disabled
-	// 									value="none"
-	// 									className="selectItem"
-	// 								>
-	// 									<>Select Option</>
-	// 								</MenuItem>
-	// 								{props?.menuItems?.map((menuOption: any) => (
-	// 									<MenuItem
-	// 										value={menuOption?.ddName}
-	// 										className="selectItem"
-	// 									>
-	// 										{menuOption?.ddName}
-	// 									</MenuItem>
-	// 								))}
-	// 							</Select>
-	// 						</FormControl>
-	// 					</Grid>
-	// 				</Grid>
-	// 			</>
-	// 		)
-	// 	}
-	// }
-
-	const getDDvalue = () => {
-		let ddName = ""
-		options.forEach((option: any) => {
-			if (option.ddId === selectedId) {
-				ddName = option.ddName
-			}
-		})
-
-		return ddName;
-	}
-
-
 
 	return (
 		<>
 			<Grid container sx={{ alignItems: 'center', }} xs={12} md={4} className="input-form-control">
-				{/* <Grid item xs={12} md={6} className="single-dropdown-title">
-							<p>{props?.optionName}</p>
-						</Grid> */}
 				<Grid item xs={12} >
 					<p className="label-heading">{optionName}</p>
 					<FormControl fullWidth>
@@ -142,10 +19,9 @@ const HsddInput = (props: HsddInputProps) => {
 							sx={{ p: 0, borderRadius: 0, mb: 1 }}
 							className="inputField cutom-input-field"
 							defaultValue="none"
-							value={getDDvalue()}
+							value={selectedId}
 							onChange={(e) => {
-								console.log(e);
-								props.onChange()
+								props.onChange(e.target.value)
 							}}
 						>
 							<MenuItem
