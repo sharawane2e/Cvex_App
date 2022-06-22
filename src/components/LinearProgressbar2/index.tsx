d40cd71bd512ebe03f413a3ebb6e1c855f95e497
Gaurav Jain <gaurav.jain@e2eresearch.com>
2022-06-22 16:57:13 +0530

    update for submit button

--- src/components/LinearProgressbar2/index.tsx
+++ src/components/LinearProgressbar2/index.tsx
@@ -13,10 +13,10 @@ import { useSelector } from 'react-redux';
 // import { useSelector } from 'react-redux';
 
 type ProgressBarProps = {
-    percentage : number
+  percentage: number
 };
 
-export default function LinearProgressbar2 (props: ProgressBarProps) {
+export default function LinearProgressbar2(props: ProgressBarProps) {
 
   const { leftPanel, rightPanel } = useSelector((state: any) => state);
 
@@ -58,30 +58,30 @@ export default function LinearProgressbar2 (props: ProgressBarProps) {
     // }
   };
 
-//   const progressBarUpdateNew = (answered: number, totalQuestions: number) => {
-//     var progressPercentage = (answered / totalQuestions) * 100;
-//     //console.log('Progress Percentage', progressPercentage);
-//     return progressPercentage;
-//   };
+  //   const progressBarUpdateNew = (answered: number, totalQuestions: number) => {
+  //     var progressPercentage = (answered / totalQuestions) * 100;
+  //     //console.log('Progress Percentage', progressPercentage);
+  //     return progressPercentage;
+  //   };
 
   return (
     <>
       <Box className="topContainer">
         <Box className="leftPanel">
-        {/* <Box className="progressbar-text" sx={{ minWidth: 35 }}>
+          {/* <Box className="progressbar-text" sx={{ minWidth: 35 }}>
                         <Typography variant="body2" color="text.secondary">{`(${Math.round(props?.percentage)}%)`}</Typography>
                     </Box> */}
-            <div className="ProgressText">{"Answered " + Math.round(props?.percentage) + "%"}</div>
-            <div className="progressbar-container">
-                <Box
-                    className="progressbar-innercontainer jc-start"
-                    sx={{ display: 'flex', justifyContent: 'start' }}
-                >
-                    <Box className="progressbar" sx={{ width: '100%', mr: 1 }}>
-                        <LinearProgress variant="determinate" value={props?.percentage} />
-                    </Box>
-                </Box>
-            </div>
+          <div className="ProgressText">{"Answered " + Math.round(props?.percentage) + "%"}</div>
+          <div className="progressbar-container">
+            <Box
+              className="progressbar-innercontainer jc-start"
+              sx={{ display: 'flex', justifyContent: 'start' }}
+            >
+              <Box className="progressbar" sx={{ width: '100%', mr: 1 }}>
+                <LinearProgress variant="determinate" value={props?.percentage} />
+              </Box>
+            </Box>
+          </div>
         </Box>
 
         <Box className="rightPanel">
@@ -102,7 +102,7 @@ export default function LinearProgressbar2 (props: ProgressBarProps) {
               <CustomButton
                 className={leftPanel.categories.filter((x: any) => x.totalAnswered == x.totalQues).length == leftPanel.categories.length ? 'submitButton submitactive' : 'submitButton next-button'}
                 onClick={() =>
-                  handleBtnClick(jsonData?.data?.progressBarData?.saveBtn?.submitId)
+                  handleBtnClick(jsonData?.data?.progressBarData?.submitBtn?.submitId)
                 }
               >
                 {getParsedData(
